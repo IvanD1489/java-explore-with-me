fed9c3fbe4952b5e4308d153fac5d825b770cc1b
Ivan D <demin.ivan21@yandex.ru>
2025-06-02 17:50:10 +0300

    Code style

--- stats-client/src/main/java/ru/practicum/stats/client/StatsClient.java
+++ stats-client/src/main/java/ru/practicum/stats/client/StatsClient.java
@@ -1,13 +1,11 @@
 package ru.practicum.stats.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
